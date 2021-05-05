502d3cea87fb4f1085094afb6d24278a9d017d93
Jacob North <34386437+North15@users.noreply.github.com>
2021-05-05 14:55:52 -0400

    Started new files, and restructured menu.

---
+++ Searching.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_program_console
+{
+    class Searching
+    {
+        public void Display_Directions()
+        {
+
+            return;
+        }
+
+    }
+}
